e7818bf9ac8d06e2e7479e78f764642659676aea
Muhammad Ashraf Misran <ashrafmisran@gmail.com>
2026-07-10 04:55:35 +0800

    Fix search url

--- app/Filament/Resources/CallRecords/CallRecordResource.php
+++ app/Filament/Resources/CallRecords/CallRecordResource.php
@@ -15,6 +15,7 @@ use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Request;
 
 class CallRecordResource extends Resource
 {
